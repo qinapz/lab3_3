a8cc880a175f3cce79b58579a6505c89fc8a4c32
Schocker <adikorzeniowski@gmail.com>
2018-04-22 19:25:30 +0200

    Implement DateTimeSource as a generic singleton

---
+++ src/main/java/edu/iis/mto/time/GenericDateTimeSource.java
@@ -0,0 +1,22 @@
+package edu.iis.mto.time;
+
+import org.joda.time.DateTime;
+
+public class GenericDateTimeSource implements DateTimeSource {
+    private static DateTimeSource instance;
+    private static DateTime dateTime;
+
+    private GenericDateTimeSource() {
+        this.dateTime = new DateTime();
+    }
+
+    public DateTimeSource getInstance() {
+        if (instance == null) instance = new GenericDateTimeSource();
+        return instance;
+    }
+
+    @Override
+    public DateTime getCurrentDate() {
+        return dateTime;
+    }
+}
