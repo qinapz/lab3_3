df4f50e437efec3ddb62b8612d700f3fd12e3991
Schocker <adikorzeniowski@gmail.com>
2018-04-23 18:28:56 +0200

    Refactor OrderTest

--- src/test/java/edu/iis/mto/time/OrderTest.java
+++ src/test/java/edu/iis/mto/time/OrderTest.java
@@ -6,18 +6,16 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class OrderTest {
-    private Order order;
-    private OrderItem orderItem;
 
     @Test(expected = OrderExpiredException.class)
     public void confirmWithExpiredOrderShouldThrowOrderExpiredException() {
-        order = new Order();
-        orderItem = new OrderItem();
+        Order order = new Order();
+        OrderItem orderItem = new OrderItem();
 
         order.setDateTimeSource(GenericDateTimeSource.getInstance());
         order.addItem(orderItem);
 
-        order.getDateTimeSource().forwardTime(26);
+        order.getDateTimeSource().forwardTime(25);
 
         order.submit();
         order.confirm();
